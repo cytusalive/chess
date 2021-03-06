388047e47f895a0260a3a65f342e2ec637b7aeda
Chocological <cytusomega@gmail.com>
2021-03-05 23:15:10 -0800

    load position

--- chess.py
+++ chess.py
@@ -17,12 +17,42 @@ clock = pygame.time.Clock()
 DARK_SQUARE = (100, 40, 20)
 LIGHT_SQUARE = (200, 180, 120)
 
+class Pieces:
+    def __init__(self):
+        self.dict = {}
+    
+    def load_pieces(self):
+        piece_types = ['r', 'n', 'b', 'q', 'k', 'p']
+        for ptype in piece_types:
+            self.dict['w'+ptype] = pygame.image.load('w'+ptype+'.png')
+            self.dict['b'+ptype] = pygame.image.load('b'+ptype+'.png')
+
 class Chessboard:
-    def __init__(self, area, position='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq'):
+    def __init__(self, area):
         self.area = area
         self.board = ['' for i in range(64)]
         self.screen_x, self.screen_y = pygame.display.get_window_size()
         self.square = pygame.Surface((self.screen_x/8, self.screen_y/8)) 
+
+    def load_position(self, position='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq'):
+        board_index = 0
+        for fen_index in range(len(position)):
+            if position[fen_index].isnumeric():
+                for l in range(int(position[fen_index])):
+                    self.board[board_index] = ''
+                    board_index += 1
+            elif position[fen_index] == '/':
+                continue
+            elif position[fen_index] == ' ':
+                break
+            else:
+                if position[fen_index].islower():
+                    self.board[board_index] = 'b' + position[fen_index].upper()
+                    board_index += 1
+                else:
+                    self.board[board_index] = 'w' + position[fen_index]
+                    board_index += 1
+            
               
     def draw(self):
         for y in range(8):
@@ -40,11 +70,13 @@ class Chessboard:
 
                 self.area.blit(self.square, (self.screen_x/8 * x, self.screen_y/8 * y))
 
-board = Chessboard(screen)
+chessgame = Chessboard(screen)
+chessgame.load_position()
+print(chessgame.board)
 
 while True:
     screen.fill(bgcolor)
-    board.draw()
+    chessgame.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
